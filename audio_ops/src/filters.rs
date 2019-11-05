447cd054f79aaae5bf9b8ad837599f2f421b22f8
Ruslan Prokopchuk <fer.obbee@gmail.com>
2019-11-06 07:00:38 +1100

    add iir filters

---
+++ audio_ops/src/filters.rs
@@ -0,0 +1,68 @@
+//! Filters
+//!
+//! Basic IIR low/high-pass filters.
+//!
+//! Sources to connect: input, cut-off frequency.
+use audio_vm::{Frame, Op, Sample, Stack, CHANNELS};
+use itertools::izip;
+
+pub struct LPF {
+    output: Frame,
+    sample_angular_period: Sample,
+}
+
+impl LPF {
+    pub fn new(sample_rate: u32) -> Self {
+        let sample_angular_period = 2.0 * std::f64::consts::PI / Sample::from(sample_rate);
+        LPF {
+            output: [0.0; CHANNELS],
+            sample_angular_period,
+        }
+    }
+}
+
+impl Op for LPF {
+    fn perform(&mut self, stack: &mut Stack) {
+        let cut_off_freq = stack.pop();
+        let input = stack.pop();
+        for (output, &x, &frequency) in izip!(&mut self.output, &input, &cut_off_freq) {
+            let k = frequency * self.sample_angular_period;
+            let a = k / (k + 1.0);
+            *output += a * (x - *output);
+        }
+        stack.push(&self.output);
+    }
+}
+
+pub struct HPF {
+    output: Frame,
+    sample_angular_period: Sample,
+    x_prime: Frame,
+}
+
+impl HPF {
+    pub fn new(sample_rate: u32) -> Self {
+        let sample_angular_period = 2.0 * std::f64::consts::PI / Sample::from(sample_rate);
+        HPF {
+            output: [0.0; CHANNELS],
+            sample_angular_period,
+            x_prime: [0.0; CHANNELS],
+        }
+    }
+}
+
+impl Op for HPF {
+    fn perform(&mut self, stack: &mut Stack) {
+        let cut_off_freq = stack.pop();
+        let input = stack.pop();
+        for (output, &x, &frequency, x_prime) in
+            izip!(&mut self.output, &input, &cut_off_freq, &mut self.x_prime)
+        {
+            let k = frequency * self.sample_angular_period;
+            let a = 1.0 / (k + 1.0);
+            *output = a * (*output + x - *x_prime);
+            *x_prime = x;
+        }
+        stack.push(&self.output);
+    }
+}
--- audio_ops/src/lib.rs
+++ audio_ops/src/lib.rs
@@ -1,7 +1,9 @@
+mod biquad;
 mod constant;
 mod delay;
 mod envelopes;
 mod feedback;
+mod filters;
 mod function;
 mod metro;
 mod noise;
@@ -11,9 +13,8 @@ mod pulse;
 pub mod pure;
 mod sample_and_hold;
 mod stack;
-mod biquad;
 
 pub use self::{
-    constant::*, delay::*, envelopes::*, feedback::*, function::*, metro::*, noise::*, osc::*,
-    phasor::*, pulse::*, sample_and_hold::*, stack::*, biquad::*
+    biquad::*, constant::*, delay::*, envelopes::*, feedback::*, filters::*, function::*, metro::*,
+    noise::*, osc::*, phasor::*, pulse::*, sample_and_hold::*, stack::*,
 };
