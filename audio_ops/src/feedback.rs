adc3baed09a14db36be6a645af7a410855fde83c
Ruslan Prokopchuk <fer.obbee@gmail.com>
2019-11-09 07:50:05 +1100

    fix feedback

--- audio_ops/src/feedback.rs
+++ audio_ops/src/feedback.rs
@@ -1,15 +1,19 @@
 use crate::delay::Delay;
-use audio_vm::{Op, Stack, CHANNELS};
+use audio_vm::{Frame, Op, Stack, CHANNELS};
 use itertools::izip;
 
 pub struct Feedback {
     delay: Delay,
+    delay_input: Frame,
 }
 
 impl Feedback {
     pub fn new(sample_rate: u32, max_delay: f64) -> Self {
         let delay = Delay::new(sample_rate, max_delay);
-        Feedback { delay }
+        Feedback {
+            delay,
+            delay_input: [0.0; CHANNELS],
+        }
     }
 }
 
@@ -17,18 +21,19 @@ impl Op for Feedback {
     fn perform(&mut self, stack: &mut Stack) {
         let gain = stack.pop();
         let delay = stack.pop();
-        let input = stack.peek();
+        let input = stack.pop();
 
+        stack.push(&self.delay_input);
         stack.push(&delay);
+
         self.delay.perform(stack);
 
         let delayed = stack.pop();
 
-        let mut frame = [0.0; CHANNELS];
-        for (sample, &x, &gain, &delayed) in izip!(&mut frame, &input, &gain, &delayed) {
+        for (sample, &x, &gain, &delayed) in izip!(&mut self.delay_input, &input, &gain, &delayed) {
             *sample = x + gain * delayed;
         }
 
-        stack.push(&frame);
+        stack.push(&self.delay_input);
     }
 }
