6e9b363b9efe07539ffdc9519e185df5e9487f49
Ruslan Prokopchuk <fer.obbee@gmail.com>
2020-09-06 18:20:57 +1000

    smooth out norm

--- audio_ops/src/normalise.rs
+++ audio_ops/src/normalise.rs
@@ -5,17 +5,23 @@
 //! Sources to connect: input.
 
 use crate::buffer::Buffer;
-use audio_vm::{Frame, Op, Stack, CHANNELS};
+use audio_vm::{Frame, Op, Sample, Stack, CHANNELS};
 use itertools::izip;
 
 pub struct Normalise {
     window: Buffer<Frame>,
+    min: Sample,
+    max: Sample,
+    q: Sample,
 }
 
 impl Normalise {
     pub fn new(window_size: usize) -> Self {
         Normalise {
             window: Buffer::new([0.0; CHANNELS], window_size),
+            min: 0.0,
+            max: 0.0,
+            q: (window_size as Sample).recip(),
         }
     }
 }
@@ -34,9 +40,12 @@ impl Op for Normalise {
             }
         }
 
+        self.min += self.q * (min - self.min);
+        self.max += self.q * (max - self.max);
+
         let mut frame = [0.0; CHANNELS];
         for (y, &x) in izip!(&mut frame, &input) {
-            *y = crate::pure::linlin(x, min, max, -1.0, 1.0);
+            *y = crate::pure::linlin(x, self.min, self.max, -1.0, 1.0);
         }
         stack.push(&frame);
     }
@@ -44,6 +53,8 @@ impl Op for Normalise {
     fn migrate(&mut self, other: &Box<dyn Op>) {
         if let Some(other) = other.downcast_ref::<Self>() {
             self.window.copy_backward(&other.window);
+            self.min = other.min;
+            self.max = other.max;
         }
     }
 }
